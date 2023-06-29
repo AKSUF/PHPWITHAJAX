d8fe4f8fec97ec4695d75bd5ffc9235160cdebb6
AKSUF <abu053125@gmail.com>
2023-06-29 18:50:15 +0600

    how to do live search in

--- Load/ajax-live-search.php
+++ Load/ajax-live-search.php
@@ -1,6 +1,35 @@
 <?php
+include('../CommonArear/commonfunction.php');
 $search_value=$_POST["search"];
 
+$sql = "SELECT * FROM `student` where first_name LIKE '%{$search_value}%' or last_name LIKE '%{$search_value}%'";
+$result = mysqli_query($con, $sql) or die("SQL Query Failed");
+$output="";
+if (mysqli_num_rows($result) > 0) {
+    $output = '
+    <table border="1" width="100%" cellspacing="0" cellpadding="10px">
+        <tr>
+            <th width="100px">Id</th>
+            <th>Name</th>
+       <th width="100px">Edit</th>
+       <th width="100px">Delete</th>
 
+        </tr>
+    ';
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $output .= "
+        <tr>
+            <td>{$row['id']}</td>
+            <td>{$row['first_name']} {$row['last_name']}</td> <td><button class='edit-btn' data-eid='{$row["id"]}'>Edit</button></td><td><button class='delete-btn' data-id='{$row["id"]}'>Delete</button></td>
+        </tr>";
+    }
+
+    $output .= '</table>';
+    mysqli_close($con);
+    echo $output;
+} else {
+    echo "<h2>No records found</h2>";
+}
 
 ?>
