05e22af5acb86dbf396d7c9533f9e7e26f1a31ea
AKSUF <abu053125@gmail.com>
2023-07-01 08:49:02 +0600

    how to read data by json formate

---
+++ Restpart/api_fetch.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type:application/json');
+header('Acess-Control-Allow-Origin:*');
+include('../CommonArear/commonfunction.php');
+$sql="select * from student";
+$result=mysqli_query($con,$sql)or die ("SQL Query Failed");
+if(mysqli_num_rows($result)>0){
+$output=mysqli_fetch_all($result,MYSQLI_ASSOC);
+echo json_encode($output);
+}else{
+    echo json_encode(array('message'=>'No Record Found','status'=>false));
+
+}
+
+
+?>
