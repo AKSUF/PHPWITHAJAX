c64815a7b8061b32161663ddcb37f1234f5d2380
AKSUF <abu053125@gmail.com>
2023-07-02 09:26:07 +0600

    gfghfdhdfgds

---
+++ Corona/js/index.js
@@ -0,0 +1,22 @@
+// $.ajax({
+//     url: "https://sales-tax-rates1.p.rapidapi.com/v/api/?zip=99502",
+//     type: "GET",
+//     dataType: "JSON",
+//     success: function(data) {
+//         console.log(data);
+//     }
+// })$(document).ready(function() {
+const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=csv&unitGroup=us&shortColumnNames=0',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '6b21896eadmshc3f91162435c5bcp15e9abjsnb9c617ad4e50',
+        'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+    }
+};
+
+$.ajax(settings).done(function(response) {
+    console.log(response);
+});
---
+++ Corona/phphtml/index.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="../js/index.js"></script>
+</body>
+</html>
